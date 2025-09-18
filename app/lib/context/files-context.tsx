515cb2da1e59055aba51a8998bf0507e10a53bcc
zulfikarrosadi <zulfikarrosadi31@gmail.com>
2025-09-18 20:45:31 +0700

    feat(file): add file context to store opfs data

---
+++ app/lib/context/files-context.tsx
@@ -0,0 +1,31 @@
+import { createContext, useContext, useState, type Dispatch, type JSX, type ReactNode, type SetStateAction } from "react";
+
+type File = {
+  name: string;
+  // handle: FileSystemFileHandle;
+  // kind: string;
+  // lastModified: number;
+  relativePath: string;
+  size: number;
+  type: string;
+}
+
+
+export const FileContext = createContext<{
+  files: File[],
+  setFiles: Dispatch<SetStateAction<File[]>>
+}>({ files: [], setFiles: () => { } })
+
+export function FileProvider({ children }: { children: JSX.Element | ReactNode }) {
+  const [files, setFiles] = useState<File[]>([])
+
+  return (
+    <FileContext value={{ files, setFiles }}>
+      {children}
+    </FileContext>
+  )
+}
+
+export function useFileContext() {
+  return useContext(FileContext)
+}
--- app/routes/root-layout.tsx
+++ app/routes/root-layout.tsx
@@ -4,15 +4,18 @@ import {
 } from '~/components/ui/sidebar';
 import { AppSidebar } from '~/components/app-sidebar';
 import { Outlet } from 'react-router';
+import { FileProvider } from '~/lib/context/files-context';
 
 export default function RootLayout() {
   return (
-    <SidebarProvider>
-      <AppSidebar />
-      <div className='flex justify-content-center align-items-center h-[32px] p-[6px]'>
-        <SidebarTrigger />
-      </div>
-      <Outlet />
-    </SidebarProvider>
+    <FileProvider>
+      <SidebarProvider>
+        <AppSidebar />
+        <div className='flex justify-content-center align-items-center h-[32px] p-[6px]'>
+          <SidebarTrigger />
+        </div>
+        <Outlet />
+      </SidebarProvider>
+    </FileProvider>
   )
 }
