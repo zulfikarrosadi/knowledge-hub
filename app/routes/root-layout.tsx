f6bb25d8cacd60388b0b021fce561c4fd7b5c437
zulfikarrosadi <zulfikarrosadi31@gmail.com>
2025-10-24 20:43:16 +0700

    feat: initialize sooner

--- app/routes/root-layout.tsx
+++ app/routes/root-layout.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from 'react-router';
 import { FilesProvider } from '~/lib/context/files-context';
+import { Toaster } from '~/components/ui/sonner';
 
 export default function RootLayout() {
   return (
     <FilesProvider>
+      <Toaster closeButton={true} expand={true} position='top-right' />
       <Outlet />
     </FilesProvider>
   )
